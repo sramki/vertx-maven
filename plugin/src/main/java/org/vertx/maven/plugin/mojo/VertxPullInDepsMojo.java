5b3e1998c48ead8d19708e4bdbb5b3cacff3e0b8
ramki <ramasubramanian@paypal.com>
2013-04-26 16:10:23 -0700

    Fixed pullindeps. Also no mods dir in root from now on. Its all in target/mods.

--- plugin/src/main/java/org/vertx/maven/plugin/mojo/VertxPullInDepsMojo.java
+++ plugin/src/main/java/org/vertx/maven/plugin/mojo/VertxPullInDepsMojo.java
@@ -1,22 +1,43 @@
 package org.vertx.maven.plugin.mojo;
 
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.vertx.java.platform.PlatformManager;
-
+import static java.lang.Long.MAX_VALUE;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.apache.maven.plugins.annotations.ResolutionScope.COMPILE_PLUS_RUNTIME;
 import static org.vertx.java.platform.PlatformLocator.factory;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.Handler;
+import org.vertx.java.platform.PlatformManager;
 
 @Mojo(name = "pullindeps", requiresProject = true, threadSafe = false, requiresDependencyResolution = COMPILE_PLUS_RUNTIME)
 public class VertxPullInDepsMojo extends BaseVertxMojo {
 
-  @Override
-  public void execute() throws MojoExecutionException {
-    try {
-      final PlatformManager pm = factory.createPlatformManager();
-      pm.pullInDependencies(moduleName, null);
-    } catch (final Exception e) {
-      throw new MojoExecutionException(e.getMessage());
+    @Override
+    public void execute() throws MojoExecutionException {
+        try {
+            System.setProperty("vertx.mods", "target/mods");
+            final PlatformManager pm = factory.createPlatformManager();
+
+            final CountDownLatch latch = new CountDownLatch(1);
+            pm.pullInDependencies(moduleName, new Handler<AsyncResult<Void>>() {
+                @Override
+                public void handle(final AsyncResult<Void> event) {
+                    if (event.succeeded()) {
+                        latch.countDown();
+                    } else {
+                        getLog().info(
+                                "Cannot find the module. Did you forget to do mvn package?");
+                        latch.countDown();
+                    }
+                }
+            });
+            latch.await(MAX_VALUE, MILLISECONDS);
+        } catch (final Exception e) {
+            throw new MojoExecutionException(e.getMessage());
+        }
     }
-  }
 }
