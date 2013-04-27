180621f7959e96306ffac51bc830e311759e58a8
ramki <ramasubramanian@paypal.com>
2013-04-26 17:06:21 -0700

    Added pullindeps property and also the goal to prepare-package phase.

--- plugin/src/main/java/org/vertx/maven/plugin/mojo/VertxPullInDepsMojo.java
+++ plugin/src/main/java/org/vertx/maven/plugin/mojo/VertxPullInDepsMojo.java
@@ -9,6 +9,7 @@ import java.util.concurrent.CountDownLatch;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
 import org.vertx.java.platform.PlatformManager;
@@ -16,26 +17,32 @@ import org.vertx.java.platform.PlatformManager;
 @Mojo(name = "pullindeps", requiresProject = true, threadSafe = false, requiresDependencyResolution = COMPILE_PLUS_RUNTIME)
 public class VertxPullInDepsMojo extends BaseVertxMojo {
 
+    @Parameter(property = "vertx.pullindeps", defaultValue = "false")
+    protected Boolean pullindeps;
+
     @Override
     public void execute() throws MojoExecutionException {
         try {
-            System.setProperty("vertx.mods", "target/mods");
-            final PlatformManager pm = factory.createPlatformManager();
+            if (pullindeps) {
+                System.setProperty("vertx.mods", "target/mods");
+                final PlatformManager pm = factory.createPlatformManager();
 
-            final CountDownLatch latch = new CountDownLatch(1);
-            pm.pullInDependencies(moduleName, new Handler<AsyncResult<Void>>() {
-                @Override
-                public void handle(final AsyncResult<Void> event) {
-                    if (event.succeeded()) {
-                        latch.countDown();
-                    } else {
-                        getLog().info(
-                                "Cannot find the module. Did you forget to do mvn package?");
-                        latch.countDown();
-                    }
-                }
-            });
-            latch.await(MAX_VALUE, MILLISECONDS);
+                final CountDownLatch latch = new CountDownLatch(1);
+                pm.pullInDependencies(moduleName,
+                        new Handler<AsyncResult<Void>>() {
+                            @Override
+                            public void handle(final AsyncResult<Void> event) {
+                                if (event.succeeded()) {
+                                    latch.countDown();
+                                } else {
+                                    getLog().info(
+                                            "Cannot find the module. Did you forget to do mvn package?");
+                                    latch.countDown();
+                                }
+                            }
+                        });
+                latch.await(MAX_VALUE, MILLISECONDS);
+            }
         } catch (final Exception e) {
             throw new MojoExecutionException(e.getMessage());
         }
